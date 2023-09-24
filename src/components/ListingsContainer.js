dd85b64ee18adf5e482aba137e38dc51e929d2cc
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2023-09-24 01:54:06 -0400

    added favorite capability

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,29 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useEffect, useState } from "react";
+import ListingCard from "./ListingCard";
+
 
 function ListingsContainer() {
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+      .then(r => r.json())
+      .then(listings => setListings(listings))
+  }, [])
+
+const listingCard = listings.map((listing) => (
+    <ListingCard 
+      key={listing.id}
+      listing = {listing}
+    />
+  ))
+
+
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingCard}
       </ul>
     </main>
   );
